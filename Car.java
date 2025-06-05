a2a6b1f70c8fc15ba711b10d7ff92c6f51eea952
Nick-v-d-v <1210727@student.roc-nijmegen.nl>
2025-06-05 17:53:12 +0200

    Constructor gemaakt voor car
    dit zocht er voor dat je metten kan aangeven welkt merk enzo het is

--- Car.java
+++ Car.java
@@ -1,5 +1,15 @@
 package herkansingOop1;
 
 public class Car {
-
+	private String licensePlate;
+	private String brand;
+	private String model;
+	private String colour;
+	
+	public Car(String licensePlate, String brand, String model, String colour) {
+		this.licensePlate = licensePlate;
+		this.brand = brand;
+		this.model = model;
+		this.colour = colour;
+	}
 }
--- License.java
+++ License.java
@@ -1,5 +1,5 @@
 package herkansingOop1;
 
 public class License {
-	System.out.println("Hello World!");
+
 }
