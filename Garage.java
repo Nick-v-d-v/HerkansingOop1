63c651c8cab0f8dc98ed409484c9d18ed0592e96
Nick-v-d-v <1210727@student.roc-nijmegen.nl>
2025-06-05 23:27:19 +0200

    Begin garage class
    eerste 4 stappen van de garage stuk van de opdracht gemaakt

--- Garage.java
+++ Garage.java
@@ -1,5 +1,17 @@
 package herkansingOop1;
 
-public class Garage {
+import java.util.ArrayList;
 
+public class Garage {
+	public static int uniqueId = 0;
+	public ArrayList<Car> parkedCars;
+	public int id;
+	public int maxCapacity;
+	
+	public Garage(int maxCapacity) {
+		uniqueId++;
+		this.id = uniqueId;
+		this.maxCapacity = maxCapacity;
+		this.parkedCars = new ArrayList<Car>();
+	}
 }
