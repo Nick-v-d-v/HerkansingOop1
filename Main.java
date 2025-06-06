38f2a5349a93748e16a80b5fb0c08766354c4804
Nick-v-d-v <1210727@student.roc-nijmegen.nl>
2025-06-06 17:44:23 +0200

    List van licenses aanmaken
    Maakt een lijst en voegt licenses toe voor cars met toegang tot specifieke garages.

--- Main.java
+++ Main.java
@@ -10,12 +10,16 @@ public class Main {
 		ArrayList<Car> cars = new ArrayList<>();
 		cars.add(new Car("AA-11-BB", "Opel", "Zafira", "Black"));
 		cars.add(new Car("CC-22-DD", "Toyota", "Corolla", "White"));
-		cars.add(new Car("EE-33-FF", "Ford", "Focus", "Blue"));
-		
+		cars.add(new Car("EE-33-FF", "Ford", "Focus", "Blue"));	
 		
 		ArrayList<Garage> garage = new ArrayList<>();
 		garage.add(new Garage(2));
 		garage.add(new Garage(3));
+		
+		ArrayList<License> licenses = new ArrayList<>();
+		licenses.add(new License(cars.get(0), garage.get(0).GetId()));
+		licenses.add(new License(cars.get(1), garage.get(0).GetId()));
+		licenses.add(new License(cars.get(2), garage.get(1).GetId()));
 	}
 
 }
